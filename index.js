99722ea6a9f7d950831e4bb6cc43131152a8f65d
Fatima <fatergesh@gmail.com>
2024-10-03 19:30:08 +0600

    lesson-1

---
+++ index.js
@@ -0,0 +1,69 @@
+// alert("Hello Maralbek");
+
+// confirm("Do you want to continue?");
+// prompt("What is your name?");
+// console.log("Hello Beknazar");
+// console.log(3 + 7);
+// console.log(8 * 6);
+// console.log(15/3);
+// console.log(21-14);
+// console.log("7"+9);
+// console.warn("this is a simple text");
+// console.error("this is an error");
+// console.log(active);
+
+// переменные:
+// let
+// var
+// const
+
+// let user = "Kutman"
+// user = "Azim"
+// console.log(user);
+// let age = user
+// console.log(age);
+// age = 16 
+
+//   туура билдируу
+// let user;
+// let state123;
+// let $info;
+// let _about;
+// let my_name;
+// let karaKol;
+// let myName = 'Dilmurad', age = 16, city = 'JA'
+//  myName = 'Fatima Tursunali kyzy', age = 18, city = 'Osh'
+
+// console.log(myName);
+// console.log(age);
+// console.log(city);
+
+
+
+//   туура эмес билдируу
+//   let 123state;
+//   let my-name;
+//   let kara kol;
+// let sdnjdfhej = 'beka'
+// let let;
+// let for;
+// let break;
+
+// var myAge = 34;
+// myAge = 18;
+// console.log(myAge);
+
+// const about = 'Okurmen'
+// console.log(about);
+// const number = 18;
+// const good = 3;
+// const sum = number * good
+// console.log((number / good) * sum);
+
+const a = 300;
+const b = 441;
+console.log(S=(a*b));       
+
+console.log(P=(a+b)*2);
+
+
